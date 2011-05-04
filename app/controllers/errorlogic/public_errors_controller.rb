250409070ade7a8e046b67d9927e1aff4ed1ee78
Cyril Picard <Cyril@picard.ch>
2011-05-04 08:08:03 +0200

    Corrected the main code and added all error views.

--- app/controllers/errorlogic/public_errors_controller.rb
+++ app/controllers/errorlogic/public_errors_controller.rb
@@ -1,4 +1,22 @@
 class Errorlogic::PublicErrorsController < ApplicationController
+  # This is just a very basic controller
+  # For custom methods just create our own PublicErrorsController which subclasses this one
+  
+  
+  
+  # 404 error
   def not_found
+  end
+  
+  def conflict
+  end
+  
+  def not_implemented
+  end
+  
+  def method_not_allowed
+  end
+  
+  def unprocessable_entity
   end
 end
--- lib/action_dispatch/show_exceptions.rb
+++ lib/action_dispatch/show_exceptions.rb
@@ -1,29 +1,29 @@
 module ActionDispatch
+  # We subclass this middleware to render nice dynamic error pages instead of classical 404.html,... files
   class ShowExceptions
     private
+    # By default render_exception doesn't send the request object to rescue_action_in_public.
+    # Here we just store the env variable in a class variable so we can retrieve it later.
     def render_exception(env, exception)
-      log_error(exception)
-
-      request = Request.new(env)
-      if @consider_all_requests_local || request.local?
-        rescue_action_locally(request, exception)
-      else
-        rescue_action_in_public(request, exception)
-      end
-    rescue Exception => failsafe_error
-      $stderr.puts "Error during failsafe response: #{failsafe_error}\n  #{failsafe_error.backtrace * "\n  "}"
-      FAILSAFE_RESPONSE
+      @env = env
+      # Original rails code is always best
+      super(env,exception)
     end
     
-    def rescue_action_in_public(request, exception)
+    
+    def rescue_action_in_public(exception)
+      request = Request.new(@env)
       status = status_code(exception)
-      if status == 404
-        action = @@rescue_responses[exception.class.name]
-        response = Errorlogic::PublicErrorsController.action(action).call(request.env).last
-        render(status, response.body)
-      else
-        super(exception)
+      action = @@rescue_responses[exception.class.name]
+      controller = PublicErrorsController || Errorlogic::PublicErrorsController;
+      response = controller.action(action).call(request.env).last
+      render(status, response.body)
       end
+    # Any exceptions results in calling the parent method
+    rescue Exception => e
+      log_error(e)
+      super(excpetion)
     end
+    
   end
 end
