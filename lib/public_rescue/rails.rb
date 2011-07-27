3e50a85d2eae5556af915ac060cda717c3e1368b
Cyril Picard <Cyril@picard.ch>
2011-07-27 10:04:08 +0200

    little typo

--- lib/public_rescue/rails.rb
+++ lib/public_rescue/rails.rb
@@ -7,7 +7,7 @@ module PublicRescue
         config.public_rescue.controller = :public_errors
         
         initializer "devise.add_middleware" do |app|
-            app.middleware.swap ActionDispatch::ShowExceptions, PublicRescue::ShowExceptions, app.consider_all_requests_local
+            app.middleware.swap ActionDispatch::ShowExceptions, PublicRescue::ShowExceptions, app.config.consider_all_requests_local
         end
         
         
