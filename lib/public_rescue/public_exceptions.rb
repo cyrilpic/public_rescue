f90660bf4c086c371faaba8479e05f775f961608
Cyril Picard <Cyril@picard.ch>
2012-01-25 12:11:37 +0100

    Changed middleware to support Rails 3.2

---
+++ lib/public_rescue/public_exceptions.rb
@@ -0,0 +1,37 @@
+module PublicRescue
+  class PublicExceptions
+    
+    def call(env)
+      
+      exception = env["action_dispatch.exception"]
+      wrapper = ActionDispatch::ExceptionWrapper.new(env, exception)
+      
+      request = ActionDispatch::Request.new(env)
+      status = wrapper.status_code
+      exception_details = {
+              :request => request, :exception => exception,
+              :status => status,
+              :application_trace => wrapper.application_trace,
+              :framework_trace => wrapper.framework_trace,
+              :full_trace => wrapper.full_trace
+      }
+      request.env['public_rescue.exception_details'] = exception_details
+      action = ActionDispatch::ExceptionWrapper.rescue_responses[exception.class.name]
+      controller_name = Rails.configuration.public_rescue.controller.to_s.camelize << 'Controller'
+      begin
+        controller = Rails.const_get(controller_name)
+      rescue NameError => n_E
+        controller = PublicErrorsController
+      end
+      response = controller.action(action).call(request.env).last
+      render(status, response.body)
+      
+    end
+    
+    private
+    
+    def render(status, body)
+      [status, {'Content-Type' => "text/html; charset=#{Response.default_charset}", 'Content-Length' => body.bytesize.to_s}, [body]]
+    end
+  end
+end
--- lib/public_rescue/rails.rb
+++ lib/public_rescue/rails.rb
@@ -6,8 +6,12 @@ module PublicRescue
         config.public_rescue = ActiveSupport::OrderedOptions.new
         config.public_rescue.controller = :public_errors
         
-        initializer "devise.add_middleware" do |app|
-            app.middleware.swap ActionDispatch::ShowExceptions, PublicRescue::ShowExceptions, app.config.consider_all_requests_local
+        if Rails.VERSION.MINOR == 2
+          config.exceptions_app = PublicExceptions
+        else
+          initializer "public_rescue.add_middleware" do |app|
+              app.middleware.swap ActionDispatch::ShowExceptions, PublicRescue::ShowExceptions, app.config.consider_all_requests_local
+          end
         end
         
         
