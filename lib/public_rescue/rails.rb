67c38c7830e5d199a0f2794d6ee1fb0472b2c1fd
Cyril Picard <Cyril@picard.ch>
2012-01-25 12:15:43 +0100

    Specifying namespace for PublicExceptions class.

--- lib/public_rescue/rails.rb
+++ lib/public_rescue/rails.rb
@@ -7,7 +7,7 @@ module PublicRescue
         config.public_rescue.controller = :public_errors
         
         if Rails::VERSION::MINOR == 2
-          config.exceptions_app = PublicExceptions
+          config.exceptions_app = PublicRescue::PublicExceptions
         else
           initializer "public_rescue.add_middleware" do |app|
               app.middleware.swap ActionDispatch::ShowExceptions, PublicRescue::ShowExceptions, app.config.consider_all_requests_local
